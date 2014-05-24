1b1c53a9b1f8089552e727327745109a43aed0a3
ramsys <ramborghacklab@gmail.com>
2014-05-24 20:15:22 +0200

    Init commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='FlaskDo',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='OpenShift App and FlaskDo',
+      author='ramsys',
+      author_email='ramborghacklab@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask>=0.7.2', 'MarkupSafe'],
      )
